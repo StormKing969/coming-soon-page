537dfef64878cee3411274da4e19e8282a83e9c3
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-06-19 14:26:21 -0500

    mobile version complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Home from './components/Home/Home';
+import Home from '../Home/Home';
+
+import './App.scss';
 
 function App() {
   return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import Icon from '../../images/logo.svg';
+import MobileUserImg from '../../images/hero-mobile.jpg';
+import DesktopUserImg from '../../images/hero-desktop.jpg';
+import BtnArrow from '../../images/icon-arrow.svg';
+import BtnError from '../../images/icon-arrow.svg';
+
 import './Home.scss';
 
 const Home = () => {
   return (
     <main className="home_container">
       <div className="logo">
-
+        <img src={Icon} alt="logo" />
       </div>
 
       <div className="user_image">
+      <img src={MobileUserImg} alt="user_image" />
       </div>
 
       <div className="home_content">
-        <h1>WE'RE COMING SOON</h1>
+        <h1><span className="base_color">WE'RE </span> COMING SOON</h1>
 
-        <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launh deals</p>
+        <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals</p>
 
-        <div className="email_form">
-          <input></input>
-          <button></button>
-        </div>
+        <form className="email_form">
+          <input type="email" className="customer_input" placeholder="Email Address" autocomplete="off" required></input>
+          <button>
+            <img src={BtnArrow} alt="submit arrow" />
+          </button>
+        </form>
       </div>
     </main>
   )
