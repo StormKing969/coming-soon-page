135222d2203fad213227a45372f5d18c057f5eb1
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-06-19 01:11:50 -0500

    initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <main className="home_container">
+      <div className="logo">
+
+      </div>
+
+      <div className="user_image">
+      </div>
+
+      <div className="home_content">
+        <h1>WE'RE COMING SOON</h1>
+
+        <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launh deals</p>
+
+        <div className="email_form">
+          <input></input>
+          <button></button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Home
